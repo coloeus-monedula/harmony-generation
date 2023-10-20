22e47f96935ef87dbd2f34d7d60e31dea8451828
page <corvidaecorvus@outlook.com>
2023-10-20 19:18:45 +0100

    refactor to allow for customisability in realisastion via cmd args

--- manual_harmony.py
+++ manual_harmony.py
@@ -57,27 +57,79 @@ def convert_music21(score_path):
 # def fb_realisation_harpsichord():
 
 
-def fb_realisation_satb(voices, melody, maxpitch, score_parts, rules_args = None):
+def fb_realisation_satb(voices, maxpitch, score_parts, rules_args = None):
     bass_fb = voices["fb"]
+    voices.pop("fb")
     fb = figuredBass.realizer.figuredBassFromStream(bass_fb)
 
     # changing rules
     # http://www.choraleguide.com/vl-spacing.php ?
-    fb_rules = figuredBass.rules.Rules()
+    fb_rules = None
 
-    # typically supposed to have this be 12 but this with maxPitch often makes 0 realisations
-    fb_rules.upperPartsMaxSemitoneSeparation = None
-    
-    fb_rules.partMovementLimits = [ (1,5), (2, 14), (3, 14)]
+    if rules_args is not None:
+        fb_rules = figuredBass.rules.Rules()
 
-    # more restrictive
-    # fb_rules.partMovementLimits = [ (1,4), (2, 12), (3, 12)]
-    fb_rules.applyConsecutivePossibRulesToResolution = True
-    fb_rules.applySinglePossibRulesToResolution = True
+        # typically supposed to have this be 12 but this with maxPitch often makes 0 realisations
+        fb_rules.upperPartsMaxSemitoneSeparation = rules_args["separation"]
+        
+        fb_rules.partMovementLimits = rules_args["move_lim"]
+
+        # more restrictive
+        # fb_rules.partMovementLimits = [ (1,4), (2, 12), (3, 12)]
+        fb_rules.applyConsecutivePossibRulesToResolution = rules_args["consecutive"]
+        fb_rules.applySinglePossibRulesToResolution = rules_args["single"]
 
     # editing original soprano part
-    soprano = voices["s"]
-    incomplete_bar = soprano.measure(0)
+    for key,part in voices.items():
+        # deals with the first bar (if incomplete) so it aligns with realised values
+        handle_anacrusis(part)
+
+    # lowestPitch = sorted(soprano.pitches)[0]
+
+    # find highest pitch in soprano part as we have to generate all satb parts
+    # and hope that alto and tenor don't cross soprano part (which in practice alto part does)
+    if maxpitch == "s":
+        # highest pitch becomes highest pitch in the soprano part
+        highestPitch = sorted(voices["s"].pitches)[-1]
+    else:
+        highestPitch = maxpitch
+
+    # have to do 4 parts, else 0 solutions
+    # NOTE: if no realisations loosen upperMaxSemitone then partMovementLimits, then others 
+    realisation = fb.realize( maxPitch=highestPitch, fbRules=fb_rules)
+ 
+    realisation.keyboardStyleOutput = False
+    realised_score = realisation.generateRandomRealization()
+
+    # add voice tag
+    for part in realised_score.parts:
+        part.insert(0, instrument.Choir())
+
+    # adding OG part and potentially removing realised part
+    to_replace = []
+    partIndexes = {
+        "s":0,
+        "a":1,
+        "t":2
+    }
+    # get all the parts to replace
+    for part in score_parts["remove"]:
+        index = partIndexes[part]
+        to_replace.append(realised_score.parts[index])
+
+    # adds original Voice
+    # put at the top
+    for part in score_parts["add"]:
+        to_insert = voices[part]
+        realised_score.insert(-1, to_insert)
+
+    for to_remove in to_replace:
+        realised_score.remove(to_remove)
+
+    realised_score.show()
+
+def handle_anacrusis(part):
+    incomplete_bar = part.measure(0)
     if (incomplete_bar is not None):
         # increases each Note's offset in the first bar so rests are on left side
         offset = incomplete_bar.paddingLeft
@@ -85,37 +137,9 @@ def fb_realisation_satb(voices, melody, maxpitch, score_parts, rules_args = None
             n.offset = n.offset + offset
 
         # increase measure number to 1 if the first bar is incomplete, to match w realised score
-        for m in soprano.getElementsByClass("Measure"):
+        for m in part.getElementsByClass("Measure"):
             new_num = m.number + 1
             m.number = new_num
-
-    # find highest pitch in soprano part as we have to generate all satb parts
-    # and hope that alto and tenor don't cross soprano part (which in practice alto part does)
-    # lowestPitch = sorted(soprano.pitches)[0]
-    highestPitch = sorted(soprano.pitches)[-1]
-
-    # have to do 4 parts, else 0 solutions
-    # NOTE: if no realisations loosen upperMaxSemitone then partMovementLimits, then others 
-    realisation = fb.realize( maxPitch=highestPitch, fbRules=fb_rules)
- 
-    realisation.keyboardStyleOutput = False
-
-    # TODO: have check for if 0 realisations - gradually loosen rules until solution found
-    # TODO: and/or transpose soprano an octave up?
-    realised_score = realisation.generateRandomRealization()
-    # add voice
-    for part in realised_score.parts:
-        part.insert(0, instrument.Choir())
-
-
-
-    realised_soprano = realised_score.parts[0]
-
-    # put at the top
-    realised_score.insert(-1, soprano)
-    realised_score.remove(realised_soprano)
-
-    realised_score.show()
 
     # TODO: replace with old bass
 
@@ -150,48 +174,63 @@ def main():
 
     rules_args = None
     # adds rules to a dict
-    if (args["no-rules"] == False):
+    if (args.no_rules == False):
         rules_args = {}
 
-        if args["parts-sep"] == 0:
-            rules_args.separation = None
+        if args.parts_sep == 0:
+            rules_args["separation"] = None
         else:
-            rules_args.separation = args["parts-sep"]
+            rules_args["separation"] = args.parts_sep
 
-        rules_args.consecutive = args["ncr"]
-        rules_args.single = args["nsr"]
+        rules_args["consecutive"] = args.no_consec_rules
+        rules_args["single"] = args.no_single_rules
 
-        if args["part-limit"] is None:
-            rules_args.move_lim = [(1,5), (2, 14), (3, 14)]
+        if args.part_move_limit is None:
+            rules_args["move_lim"] = [(1,5), (2, 14), (3, 14)]
         else:
             move_lim = []
-            for pair in args["part-limit"]:
+            for pair in args.part_move_limit:
                 move_lim.append(tuple(pair))
             
-            rules_args.move_lim = move_lim
+            rules_args["move_lim"] = move_lim
 
     # TODO: check if melody line is in compare. if so, add to compare. if not, add to replace
     # then just add as is
 
+
+    # transcribing which realised parts get replaced, which get an additional OG part and which stay the same
     score_parts = {
-        "replace" : [],
-        "compare" : []
+        "remove" : [],
+        "add" : []
     }
     melody = args.melody
-    if (melody not in args["compare"]):
-        score_parts["replace"].append(melody)
-    
-    score_parts["replace"].extend(args["replace"])
-    score_parts["compare"].extend(args["compare"])
 
-            
+    if ((args.compare is None or melody not in args.compare) and (args.replace is None or melody not in args.replace)):
+        score_parts["remove"].extend(melody)
+        score_parts["add"].extend(melody)
+
+    
+    union = set()
+    if (args.replace is not None):
+        score_parts["remove"].extend(args.replace)
+        union.update(args.replace)
+
+    if (args.compare is not None):
+        union.update(args.compare)
+
+    score_parts["add"].extend(list(union))
+    # sort now so insertion should be in SAT colour 
+    sort_order = {"s":0, "a":1, "t":2}
+    score_parts["add"].sort(key=lambda x: sort_order.get(x))
+
+    print(score_parts)
 
     # score_path = "chorales/FB_source/musicXML_master/BWV_470_FB.musicxml"
     # score_path = "chorales/FB_source/musicXML_master/BWV_3.06_FB.musicxml"
     # fb = extract_FB(score_path)
     # satb = extract_voices(score_path)
     voices = convert_music21(score_path)
-    fb_realisation_satb(voices)
+    fb_realisation_satb(voices,  args.maxpitch, score_parts, rules_args)
     
     # print(satb.get("s").show("text"))
     # print(etree.tostring(fb, encoding="unicode", pretty_print=True))
