21417d93d1dad657ff1ac8af30c917fd735908ed
page <corvidaecorvus@outlook.com>
2023-11-09 01:24:00 +0000

    add muspy patch file, make muspy the patched version for multiprocessing pytorch support

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # harmony-generation
 Final year dissertation project.
+
+Muspy needs to be patched to support multiprocessing in Pytorch datasets.
+https://webcache.googleusercontent.com/search?q=cache:https://dilankam.medium.com/creating-a-patch-from-github-pull-request-a0381fb3606d#:~:text=In%20the%20above%20screenshot%2C%20you,e-mail%20metadata%20as%20well.
+https://github.com/salu133445/muspy/issues/74 
--- RNN.py
+++ RNN.py
@@ -4,15 +4,17 @@ from manual_harmony import convert_music21
 
 # TODO: move this into a separate file later
 def preprocess(file):
-    # music = muspy.read_musicxml(file)
+    music = muspy.read_musicxml(file)
+    dataset = music.to_pytorch_dataset(representation = "pianoroll")
+
     # TODO: how to get FBs here? run it through extract baseline - get lyrics separately - rerun with "normal bass"?
 
     # TO avoid the chord issue mentioned in repo, convert to json ?
     music21_obj = convert_music21(file, return_as_score=True)
     # music21_obj.show()
 
-    music = muspy.from_music21(music21_obj.parts[-1])
-    print(music)
+    # music = muspy.from_music21(music21_obj.parts[-1])
+    # print(music)
 
 # https://github.com/magenta/note-seq
 
