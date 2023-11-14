52f1fc439c3b18954d402866324b2c54a61b7c3c
page <corvidaecorvus@outlook.com>
2023-11-14 00:56:11 +0000

    add local dataset class

--- local_datasets.py
+++ local_datasets.py
@@ -1,1 +1,57 @@
-import muspy.datasets
+from typing import Any
+import muspy.datasets as datasets
+from muspy import DatasetInfo, read_musicxml
+from muspy.music import Music
+
+
+# NOTE
+# muspy.FolderDataset.converted_exists() depends solely on a special file named .muspy.success in the folder {root}/_converted/, which serves as an indicator for the existence and integrity of the converted dataset. 
+# If the converted dataset is built by muspy.FolderDataset.convert(), the .muspy.success file will be created as well. 
+# If the converted dataset is created manually, make sure to create the .muspy.success file in the folder {root}/_converted/ to prevent errors.
+class ChoralesDataset(datasets.FolderDataset):
+    _info = DatasetInfo(license="Creative Commons Attribution 4.0 International", name="Bach Chorales Figured Bass (BCFB)", description="The complete 139 Johann Sebastian Bach chorales with figured bass encodings in MusicXML, **kern, and MEI formats, based on the Neue Bach Ausgabe (NBA) critical edition", homepage="10.5281/zenodo.5084913")
+    _extension = "musicxml"
+
+    # TODO: add FB here or later?
+    def __init__(self, root, convert=False, kind='json', n_jobs=1, ignore_exceptions=True, use_converted=None ):
+        
+        super().__init__(root, convert=convert, kind=kind, n_jobs=n_jobs, ignore_exceptions=ignore_exceptions, use_converted=use_converted )
+
+
+    # # should return the i-th data sample as a muspy.Music object
+    def __getitem__(self, index) -> Music:
+        return super().__getitem__(index)
+
+    # __len__ should return the size of the dataset
+    def __len__(self) -> int:
+        return super().__len__()
+    
+    # function based off __getitem__ code
+    def get_by_filename(self, filename) -> Music:
+        # should get single pointer back
+        filtered = [f for f in self._filenames if filename in f.__str__()]
+
+        if (len(filtered) == 0):
+            raise FileNotFoundError
+
+        if (len(filtered) > 1):
+            print("Multiple items with filename found - picking first one.")
+        
+        return self._factory(filtered[0])
+
+
+    def read(self, filename: Any) -> Music:
+        return read_musicxml(filename)
+    
+    # adds the rest of the figured bass lyrics to the bassline
+    def complete_FB_lyrics(self, music21_lyrics: zip, filename):
+        muspy_obj = self.get_by_filename(filename)
+        # muspy has the lyrics but only the first lyric obj - add the others in there
+        # iterate tuple, check inside for tuple length etc
+
+   
+
+        pass
+
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,9 @@ import glob
 from os import path, makedirs
 from extract_baseline import extract_FB
 from lxml import etree
-import muspy
 from music21 import converter, stream, chord, musicxml
-
+from local_datasets import ChoralesDataset
+from torch.utils.data import DataLoader, TensorDataset
 
 # music21 combines the two parts using chordify, takes the top layer of notes if there's a chord
 # then export to musicxml, open and grab the part in xml, replace where bass is in original score
@@ -93,7 +93,7 @@ def add_FB_to_scores(in_folder, out_folder, verbose):
 
     for f in files:
         if check_score_format(f, verbose) == True:
-            print("file: ", f)
+            # print("file: ", f)
             added_FB = extract_FB(f, use_music21_realisation=True, return_whole_scoretree=True, remove_OG_accomp=True)
 
             basename = path.basename(f)
@@ -112,36 +112,60 @@ def add_FB_to_scores(in_folder, out_folder, verbose):
 
 
 
+# folder is path to converted FB xml
+def add_FB_to_muspy_dataset(converted_folder):
+    folder_glob = path.join(converted_folder, "*.musicxml")
+    files = glob.glob(folder_glob)
+
+    chorales = ChoralesDataset(converted_folder)
+    # print(first.tracks[-1].lyrics)
+
+    # dataset = chorales.to_pytorch_dataset(representation="pianoroll")
+    # then put into dataloader?
+
+    for f in files:
+        music21_obj = converter.parseFile(f)
+        lyrics = music21_obj.parts[-1].lyrics()
+
+        # NOTE: if zip can't do None objects may show up with error if any of the keys are None
+        print(f) #pass along f as filename
+        key_num = len(lyrics.keys())
+        if key_num == 3:
+            zipped = zip(lyrics.get(1), lyrics.get(2), lyrics.get(3))
+        elif key_num == 2:
+            zipped = zip(lyrics.get(1), lyrics.get(2))
+        elif key_num == 1:
+            zipped = zip(lyrics.get(1))
+        else:
+            print ("Error: lyric keys number is ", key_num, ", defaulting to zipping 1")
+            zipped = zip(lyrics.get(1))
+
+        
+        chorales.complete_FB_lyrics(zipped, f)
+
+    return chorales
+
 def tokenise_FB():
     pass
 
 
-# folder is path to converted FB xml
-def add_tokenised_FB_dataset(converted_folder):
-    folder_glob = path.join(converted_folder, "*.musicxml")
-    files = glob.glob(folder_glob)
-
-
-    for f in files:
-        # read into muspy as musicxml USING ORIGINAL SOCRES
-        # to avoid bass splitting workaround that was needed for music21
-        # turn all into pytorch dataset - add identifier tag in class
-
-        # read lyrics in from music21 using converted folder
-        # tokenise FB
-        # piece together
-        pass
-
-
+# factory method to call, uses pianoroll conversion inside but also adds on encoded FB using tokenise_FB
+def FB_and_pianoroll():
     pass
 
 
+# TODO: save both the pre-pytorch dataset and the post pytorch painorollls
+
+def convert_to_pytorch_dataset(chorales: ChoralesDataset, pytorch_folder):
+    pass
 
 
 def main():
     in_folder = "chorales/FB_source/musicXML_master"
     out_folder = "added_FB"
-    add_FB_to_scores(in_folder, out_folder, verbose=True)
+    # add_FB_to_scores(in_folder, out_folder, verbose=True)
+
+    add_FB_to_muspy_dataset(out_folder)
 
     # file = "./chorales/FB_source/musicXML_master/BWV_248.59_FB.musicxml"
     # combine_bassvoice_accomp(file)
